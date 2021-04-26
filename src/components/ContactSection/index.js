d37379e420d0c6fe45d21c2dd022a1844d7aff8e
Bartosz Czenczek <czenczek.bartosz@gmail.com>
2021-04-26 17:43:36 +0200

    repair and add pretefier

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,17 +1,32 @@
-import AOS from 'aos';
+import AOS from "aos";
 
 const ContactSection = () => {
-    return (
-            <div className="contact_section__form_wrapper">
-                <form data-aos="slide-down" data-aos-anchor="#trigger-second">
-                    <input type="text" name="name" id="name" placeholder="Name:" required/>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" id="email" className="contact_section__email" placeholder="jan.kowalski@gmail.com"/>
-                    <textarea id="message" name="message" placeholder="wpisz wiadomość"></textarea>
-                    <input type="button" id="submit" value="Send >" className="submit-button pointer"/>
-                </form>
-            </div>
-    )
-}
+  return (
+    <div className="contact_section__form_wrapper">
+      <form data-aos="slide-down" data-aos-anchor="#trigger-second">
+        <input type="text" name="name" id="name" placeholder="Name:" required />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="contact_section__email"
+          placeholder="jan.kowalski@gmail.com"
+        />
+        <textarea
+          id="message"
+          name="message"
+          placeholder="wpisz wiadomość"
+        ></textarea>
+        <input
+          type="button"
+          id="submit"
+          value="Send >"
+          className="submit-button pointer"
+        />
+      </form>
+    </div>
+  );
+};
 
-export default ContactSection
+export default ContactSection;
